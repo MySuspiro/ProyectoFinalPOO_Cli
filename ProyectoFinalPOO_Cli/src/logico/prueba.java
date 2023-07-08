2697bef0d6b3cc3607525b09700e714745eb5a46
DELL <DELL@DESKTOP-8VSDHKO>
2023-07-08 11:00:34 -0400

    Probando si es verdad que funciona

--- ProyectoFinalPOO_Cli/src/logico/prueba.java
+++ ProyectoFinalPOO_Cli/src/logico/prueba.java
@@ -1,5 +1,21 @@
 package logico;
 
 public class prueba {
+	
+	private String probando;
+
+	public prueba(String probando) {
+		super();
+		this.probando = probando;
+	}
+
+	public String getProbando() {
+		return probando;
+	}
+
+	public void setProbando(String probando) {
+		this.probando = probando;
+	}
+	
 
 }
